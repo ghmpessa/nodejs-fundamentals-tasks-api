5bd3897ffa930f788b2c0adc905bca8610cc5563
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-14 21:45:03 -0300

    feat: add route finder to server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 import http from "node:http";
+import { json } from "./middleware/json.js";
+import { routes } from "./routes.js";
 
-const server = http.createServer(async (req, res) => {});
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await json(req, res);
+
+  const route = routes.find(
+    (route) => route.method === method && route.path === url
+  );
+
+  if (route) return route.handler(req, res);
+
+  return res.writeHead(404).end("Not found!");
+});
 
 server.listen(3333);
