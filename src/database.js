2c909070455ad046cab89a8b57531eb112bc630a
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-14 22:49:14 -0300

    feat: update database to be a json object and add update and findByID methods

--- src/database.js
+++ src/database.js
@@ -3,11 +3,11 @@ import fs from "node:fs/promises";
 const dbPath = new URL("../db.json", import.meta.url);
 
 export class Database {
-  #database = [];
+  #database = {};
 
   constructor() {
     fs.readFile(dbPath, "utf-8")
-      .then((data) => (this.#database = data))
+      .then((data) => (this.#database = JSON.parse(data)))
       .catch(() => this.#persist());
   }
 
@@ -15,29 +15,42 @@ export class Database {
     fs.writeFile(dbPath, JSON.stringify(this.#database));
   }
 
-  find() {
-    return this.#database ?? [];
+  find(table) {
+    return this.#database[table] ?? [];
   }
 
-  insert(task) {
-    if (Array.isArray(this.#database)) {
-      if (this.#database.some((item) => item["title"] === task.title))
+  findByID(table, id) {
+    return this.#database[table].find((item) => item.id === id);
+  }
+
+  insert(table, task) {
+    if (Array.isArray(this.#database[table])) {
+      if (this.#database[table].some((item) => item["title"] === task.title))
         return null;
-      this.#database.push(task);
+      this.#database[table].push(task);
     } else {
-      this.#database = [task];
+      this.#database[table] = [task];
     }
     this.#persist();
 
     return task;
   }
 
-  update(id, data) {
-    const index = this.#database.findIndex((item) => item.id === id);
+  update(table, id, data) {
+    const index = this.#database[table].findIndex((item) => item.id === id);
 
     if (index >= 0) {
-      this.#database[index] = { id, ...data };
+      const prev = this.#database[table][index];
+
+      const task = {
+        ...prev,
+        ...data,
+      };
+
+      this.#database[table][index] = task;
       this.#persist();
+
+      return task;
     }
   }
 }
